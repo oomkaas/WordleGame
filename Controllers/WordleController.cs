43f6d5b95bb500a6fba32f18d28b893a4bea9868
bbmug <bbmug@BENJI_MAN>
2024-04-05 15:08:23 +0200

    Add project files.

---
+++ Controllers/WordleController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WordleGame.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordleController : ControllerBase
+    {
+        
+        private static readonly string[] wordList = new[]
+        {
+            "football;", "rugby", "tennis", "hockey", "swimming", "basketball", "volleyball", "netball", "baseball", "wrestling", "running", "golf", "badminton"
+        };
+
+        [HttpGet]
+        [Route("generateWord")]
+        public IActionResult GenerateWord()
+        {
+            // Instantiate random number generator using system-supplied value as seed.
+            var random = new Random();
+            // Generate random indexes for types of sports
+            var randomIndex = random.Next(wordList.Length);
+            var randomWord = wordList[randomIndex];
+            Console.WriteLine($"Generated word: {randomWord}");
+
+            return Ok(randomWord);
+        }
+
+        [HttpPost]
+        [Route("checkWord")]
+        public IActionResult CheckWord([FromBody] string word)
+        {
+            if (string.IsNullOrWhiteSpace(word))
+                return BadRequest("Invalid input: word cannot be empty.");
+
+            var normalizedWord = word.Trim().ToLower();
+
+            if (!wordList.Contains(normalizedWord))
+                return BadRequest($"{word} is an invalid word: {word} is not in the word list.");
+
+            return Ok($"{word} is a valid word!");
+        }
+    }
+}
