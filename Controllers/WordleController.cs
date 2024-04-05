b3ac51f9a2e7cec0d91bd399d34fd6f3889cd1be
bbmug <bbmug@BENJI_MAN>
2024-04-05 15:50:51 +0200

    comments added

--- Controllers/WordleController.cs
+++ Controllers/WordleController.cs
@@ -22,23 +22,25 @@ namespace WordleGame.Controllers
             // Generate random indexes for types of sports
             var randomIndex = random.Next(wordList.Length);
             var randomWord = wordList[randomIndex];
-            Console.WriteLine($"Generated word: {randomWord}");
-
-            return Ok(randomWord);
+            //returns random word from predefined list
+            return Ok($"Generated word: {randomWord}");
         }
 
         [HttpPost]
         [Route("checkWord")]
         public IActionResult CheckWord([FromBody] string word)
         {
+            //checks if submitted word is empty
             if (string.IsNullOrWhiteSpace(word))
                 return BadRequest("Invalid input: word cannot be empty.");
 
             var lowerWord = word.ToLower();
 
+            //checks if word is not in predefined list
             if (!wordList.Contains(lowerWord))
                 return BadRequest($"{word} is an invalid word: {word} is not in the word list.");
 
+            //returns is word is valid
             return Ok($"{word} is a valid word!");
         }
     }
